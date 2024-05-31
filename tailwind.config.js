be1192563c9929f1ea88f936d9e9c705f470ff28
shlayb <ayubisal1345@gmail.com>
2024-05-31 17:00:17 +0700

    Initial commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./public/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      aspectRatio: {
+        '4/3': '4 / 3',
+      },
+    },
     fontFamily: { Poppins: ['Poppins', 'sans-serif'] },
   },
   plugins: [],
